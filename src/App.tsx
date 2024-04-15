c274cb4cea3d40df78411aedd0f0995326716312
Kaique Araujo <kaique.earaujo@outlook.com>
2024-04-15 17:13:18 -0300

    estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, } from 'react-native'
+
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+
+      <SafeAreaView>
+
+        <View>
+          <Text> Teste </Text> 
+        </View>
+      
+      </SafeAreaView>
+
+    )
+
+  }
+
+}
