8fb518c0f5d3ab73937a2b8e4906155daacd0e68
Kaique Araujo <kaique.earaujo@outlook.com>
2024-04-19 16:08:06 -0300

    Adição do tema, imagem, e escolha do nome receita

--- src/screens/inserirReceita.js
+++ src/screens/inserirReceita.js
@@ -1,10 +1,164 @@
 import React, {Component} from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, Dimensions , Image } from 'react-native'
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import imagem from '../../assets/imgs/addReceita.png'
+
 
 export default class App extends Component {
-    render() {
-        return (
-            <Text> A </Text>
-        )
-    }
+
+  state = {
+    inputReceita: '',
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+
+    return (
+
+      <SafeAreaView style={styleApp.app}>
+
+        <View style={styleApp.appMain}>
+
+          { /* Imagem superior */ }
+          <View style={styleApp.imagem}>
+            <Image source={imagem} style={styleApp.image}/>
+
+          </View>
+
+          { /* Input para inserir nome da receita */ }
+          <View>
+
+            <View style={styleApp.input}> 
+                <Text style={styleApp.inputDesc}> Digite o </Text>
+                <Text style={[styleApp.inputDesc, {color: '#ECA457'} ]}>nome </Text>
+                <Text style={styleApp.inputDesc}>da sua </Text>
+                <Text style={[styleApp.inputDesc, {color: '#ECA457'} ]}>receita </Text>
+            </View>
+
+            <View style={{alignItems: 'center', paddingTop: 10}}> 
+                <View style={styleApp.inputNomeReceita}>
+                    <TouchableOpacity>
+                        <TextInput 
+                            placeholder="Insira o nome aqui"
+                            style={{fontSize: 16}}
+                            value={this.state.inputNomeReceita}
+                            onChangeText={this.setInputReceita}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+          </View>
+
+          { /* Escolha da categoria */ }
+
+          <View style={styleApp.input}>
+             <Text style={styleApp.inputDesc}> Escolha uma </Text>
+             <Text style={[styleApp.inputDesc, {color: '#ECA457'} ]}>categoria </Text>
+          </View>
+
+
+          { /*  Botões de navegação */ }
+          <TouchableOpacity style={[styleApp.backButton]}
+            activeOpacity={0.7}
+            onPress={() => this.props.navigation.goBack()} >
+            <Ionicons name="arrow-back" size={30} color={'white'} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styleApp.addButton]}
+            activeOpacity={0.7}
+            onPress={() => this.props.navigation.navigate('Receita', { view: 'Home' } )} >
+            <Ionicons name="arrow-forward" size={30} color={'white'} />
+          </TouchableOpacity>
+
+
+        </View>
+      
+      </SafeAreaView>
+
+    )
+
+  }
+
 }
+
+const styleApp = StyleSheet.create({
+
+  app: {
+    flex: 1,
+    backgroundColor: '#D5ECF0',
+  },
+
+  appMain: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 20
+  },
+
+  imagem: {
+    alignItems: 'center'
+  },
+
+  image: {
+    height: (Dimensions.get('window').width / 4) * 3,
+    resizeMode: 'contain',
+  },
+
+  iconeCamera: {
+    position: 'absolute',
+    top: 230,
+    right: 50,
+
+  },
+
+  input: {
+    justifyContent: 'center',
+    marginTop: 20,
+    flexDirection: 'row'
+  },
+
+  inputDesc: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 20
+  },
+
+  inputNomeReceita: {
+    width: '80%' ,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 7,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+
+  backButton: {
+      position: 'absolute',
+      left: 30,
+      bottom: 30,
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center' ,
+      backgroundColor: '#ECA457',
+      //borderColor: '#65A8F0',
+      //borderWidth: 1
+  },
+
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center' ,
+    backgroundColor: '#ECA457',
+   }
+
+})
